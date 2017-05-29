61d6944f3b3cf68aaad742fc936f963fc7c5785f
Cynthia Rais <cynthia.rais@sfr.fr>
2017-05-29 09:51:20 +0200

    Add for noise

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -52,9 +52,10 @@ class data_analysis_cells:
         print("step init")
 
         """determination of pourcentage of choice B depending on quantity of each juice"""
-        nb_choice_A, nb_choice_B = 0, 0
+
         for i in range(self.ΔA + 1):
             for j in range(self.ΔB + 1):
+                nb_choice_A, nb_choice_B = 0, 0
                 for l in range(len(self.result_one_trial[(i, j)])):
                     if len(self.result_one_trial[(i, j)][l]) != 0 and self.result_one_trial[(i, j)][l][5] == 'A':
                         nb_choice_A += 1
@@ -105,15 +106,15 @@ class data_analysis_cells:
             for i in range(self.ΔA + 1):
                 for j in range(self.ΔB + 1):
                     for choice_i in self.list_choice:
-                        if len(self.result[(i, j, choice_i)][1]) == 0:
+                        if len(self.result[(i, j, choice_i)][2]) == 0:
                             A_chosen_cj += 0
                             B_chosen_cj += 0
                         else:
                             if choice_i == 'A':
-                                A_chosen_cj += self.result[(i, j, choice_i)][1][k]
+                                A_chosen_cj += self.result[(i, j, choice_i)][2][k]
                                 A_nb += 1
                             else:
-                                B_chosen_cj += self.result[(i, j, choice_i)][1][k]
+                                B_chosen_cj += self.result[(i, j, choice_i)][2][k]
                                 B_nb += 1
             if A_nb == 0: A_nb = 1
             if B_nb == 0: B_nb = 1
@@ -131,30 +132,30 @@ class data_analysis_cells:
             for i in range(self.ΔA + 1):
                 for j in range(self.ΔB + 1):
                     for choice_i in self.list_choice:
-                        if len(self.result[(i, j, choice_i)][2]) == 0:
+                        if len(self.result[(i, j, choice_i)][4]) == 0:
                             chosen_value_low += 0
                             chosen_value_medium += 0
                             chosen_value_high += 0
                         else:
                             if choice_i == 'A':
                                 if i < (round(self.ΔA / 3)):
-                                    chosen_value_low += self.result[(i, j, choice_i)][2][k]
+                                    chosen_value_low += self.result[(i, j, choice_i)][4][k]
                                     low_cv += 1
                                 elif (round(self.ΔA / 3)) < i < (round(self.ΔA * 2 / 3)):
-                                    chosen_value_medium += self.result[(i, j, choice_i)][2][k]
+                                    chosen_value_medium += self.result[(i, j, choice_i)][4][k]
                                     medium_cv += 1
                                 else:
-                                    chosen_value_high += self.result[(i, j, choice_i)][2][k]
+                                    chosen_value_high += self.result[(i, j, choice_i)][4][k]
                                     high_cv += 1
                             else:
                                 if j < (round(self.ΔB / 3)):
-                                    chosen_value_low += self.result[(i, j, choice_i)][2][k]
+                                    chosen_value_low += self.result[(i, j, choice_i)][4][k]
                                     low_cv += 1
                                 elif (round(self.ΔB / 3)) < j < (round(self.ΔB * 2 / 3)):
-                                    chosen_value_medium += self.result[(i, j, choice_i)][2][k]
+                                    chosen_value_medium += self.result[(i, j, choice_i)][4][k]
                                     medium_cv += 1
                                 else:
-                                    chosen_value_high += self.result[(i, j, choice_i)][2][k]
+                                    chosen_value_high += self.result[(i, j, choice_i)][4][k]
                                     high_cv += 1
             self.mean_low_cv.append(chosen_value_low / low_cv)
             self.mean_medium_cv.append(chosen_value_medium / medium_cv)
@@ -162,81 +163,125 @@ class data_analysis_cells:
         print("step 3")
         return [self.mean_low_cv, self.mean_medium_cv, self.mean_high_cv]
 
-    def average_firing_time_windows(self):
+    def average_firing_time_windows_ov(self):
         """ average of the firing rate of all the cells for certain offers betwwen 1s and 1.5s for OV and CJ cells and 1.5 and 2s for CV cells (check this)"""
         ov_A_choiceA, ov_A_choiceB, ov_B_choiceA, ov_B_choiceB = [], [], [], []
-        cjb_A_choiceA, cjb_A_choiceB, cjb_B_choiceA, cjb_B_choiceB = [], [], [], []
-        cv_A_choiceA, cv_A_choiceB, cv_B_choiceA, cv_B_choiceB = [], [], [], []
 
         #Initilisation of mean
 
-
-        mean_ov_0B1A, mean_cj_0B1A, mean_cv_0B1A = 0, 0, 0
-        mean_ov_1B0A, mean_cj_1B0A, mean_cv_1B0A = 0, 0, 0
+        mean_ov_0B1A, mean_ov_1B0A = 0, 0
 
         for choice_i in self.list_choice:
             for j in range(self.ΔB, 3, -4):
-                mean_ov_ij, mean_cjb_ij, mean_cv_ij = 0, 0, 0
-                mean_ov_ji, mean_cjb_ji, mean_cv_ji = 0, 0, 0
-                if len(self.result[(1, j, choice_i)][0]) == 0:
-                    pass
-                else:
+                mean_ov_ij, mean_ov_ji = 0, 0
+                if len(self.result[(1, j, choice_i)][0]) != 0:
                     for k in range(2000, 3001):
                         mean_ov_ij += self.result[(1, j, choice_i)][0][k]
-                        mean_cjb_ij += self.result[(1, j, choice_i)][1][k]
-                        mean_cv_ij += self.result[(1, j, choice_i)][2][k + 1000]
-                if len(self.result[(j, 1, choice_i)][0]) == 0:
-                    pass
-                else:
+                if len(self.result[(j, 1, choice_i)][0]) != 0:
                     for k in range(2000, 3001):
                         mean_ov_ji += self.result[(j, 1, choice_i)][0][k]
-                        mean_cjb_ji += self.result[(j, 1, choice_i)][1][k]
-                        mean_cv_ji += self.result[(j, 1, choice_i)][2][k + 1000]
                 if choice_i == 'A':
                     ov_A_choiceA.append(mean_ov_ij / 1000)
                     ov_B_choiceA.append(mean_ov_ji / 1000)
-                    cjb_A_choiceA.append(mean_cjb_ij / 1000)
-                    cjb_B_choiceA.append(mean_cjb_ji / 1000)
-                    cv_A_choiceA.append(mean_cv_ij / 1000)
-                    cv_B_choiceA.append(mean_cv_ji / 1000)
                 elif choice_i == 'B':
                     ov_A_choiceB.append(mean_ov_ij / 1000)
                     ov_B_choiceB.append(mean_ov_ji / 1000)
-                    cjb_A_choiceB.append(mean_cjb_ij / 1000)
-                    cjb_B_choiceB.append(mean_cjb_ji / 1000)
-                    cv_A_choiceB.append(mean_cv_ij / 1000)
-                    cv_B_choiceB.append(mean_cv_ji / 1000)
-
             if len(self.result[(1, 0, choice_i)][0]) == 0:
                 mean_ov_0B1A = 0
-                mean_cj_0B1A = 0
-                mean_cv_0B1A = 0
             else:
                 for k in range(2000, 3001):
                     mean_ov_0B1A += self.result[(1, 0, choice_i)][0][k]
-                    mean_cj_0B1A += self.result[(1, 0, choice_i)][1][k]
-                    mean_cv_0B1A += self.result[(1, 0, choice_i)][2][k + 1000]
-
             if len(self.result[(0, 1, choice_i)][0]) == 0:
                 mean_ov_1B0A = 0
-                mean_cj_1B0A = 0
-                mean_cv_1B0A = 0
             else:
                 for k in range(2000, 3001):
                     mean_ov_1B0A += self.result[(0, 1, choice_i)][0][k]
-                    mean_cj_1B0A += self.result[(0, 1, choice_i)][1][k]
-                    mean_cv_1B0A += self.result[(0, 1, choice_i)][2][k + 1000]
             if choice_i == 'A':
-                self.ov_choiceA = [mean_ov_0B1A / 1000] + ov_B_choiceA[::-1] + ov_A_choiceA + [mean_ov_1B0A / 1000]
-                self.cjb_choiceA = [mean_cj_0B1A / 1000] + cjb_B_choiceA[::-1] + cjb_A_choiceA + [mean_cj_1B0A / 1000]
-                self.cv_choiceA = [mean_cv_0B1A / 1000] + cv_B_choiceA[::-1] + cv_A_choiceA + [mean_cv_1B0A / 1000]
+                self.ov_choiceA = [mean_ov_0B1A / 1000] + ov_B_choiceA + ov_A_choiceA[::-1] + [mean_ov_1B0A / 1000]
+            else:
+                self.ov_choiceB = [mean_ov_0B1A / 1000] + ov_B_choiceB + ov_A_choiceB[::-1] + [mean_ov_1B0A / 1000]
+        print("step 4", self.ov_choiceA, self.ov_choiceB)
+        return [self.ov_choiceA, self.ov_choiceB]
+
+
+    def average_firing_time_windows_cjb(self):
+        cjb_A_choiceA, cjb_A_choiceB, cjb_B_choiceA, cjb_B_choiceB = [], [], [], []
+
+        # Initilisation of mean
+        mean_cj_0B1A, mean_cj_1B0A = 0, 0
+
+        for choice_i in self.list_choice:
+            for j in range(self.ΔB, 3, -4):
+                mean_cjb_ij, mean_cjb_ji = 0, 0
+                if len(self.result[(1, j, choice_i)][2]) != 0:
+                    for k in range(2000, 3001):
+                        mean_cjb_ij += self.result[(1, j, choice_i)][2][k]
+                if len(self.result[(j, 1, choice_i)][2]) != 0:
+                    for k in range(2000, 3001):
+                        mean_cjb_ji += self.result[(j, 1, choice_i)][2][k]
+                if choice_i == 'A':
+                    cjb_A_choiceA.append(mean_cjb_ij / 1000)
+                    cjb_B_choiceA.append(mean_cjb_ji / 1000)
+                elif choice_i == 'B':
+                    cjb_A_choiceB.append(mean_cjb_ij / 1000)
+                    cjb_B_choiceB.append(mean_cjb_ji / 1000)
+
+            if len(self.result[(1, 0, choice_i)][2]) == 0:
+                mean_cj_0B1A = 0
+            else:
+                for k in range(2000, 3001):
+                    mean_cj_0B1A += self.result[(1, 0, choice_i)][2][k]
+            if len(self.result[(0, 1, choice_i)][2]) == 0:
+                mean_cj_1B0A = 0
+            else:
+                for k in range(2000, 3001):
+                    mean_cj_1B0A += self.result[(0, 1, choice_i)][2][k]
+            if choice_i == 'A':
+                self.cjb_choiceA = [mean_cj_0B1A / 1000] + cjb_B_choiceA + cjb_A_choiceA[::-1] + [mean_cj_1B0A / 1000]
 
             else:
-                self.ov_choiceB = [mean_ov_0B1A / 1000] + ov_B_choiceB[::-1] + ov_A_choiceB + [mean_ov_1B0A / 1000]
-                self.cjb_choiceB = [mean_cj_0B1A / 1000] + cjb_B_choiceB[::-1] + cjb_A_choiceB + [mean_cj_1B0A / 1000]
-                self.cv_choiceB = [mean_cv_0B1A / 1000] + cv_B_choiceB[::-1] + cv_A_choiceB + [mean_cv_1B0A / 1000]
-        print("step 4")
-        return [self.ov_choiceA, self.cjb_choiceA, self.cv_choiceA, self.ov_choiceB, self.cjb_choiceB, self.cv_choiceB]
+                self.cjb_choiceB = [mean_cj_0B1A / 1000] + cjb_B_choiceB + cjb_A_choiceB[::-1] + [mean_cj_1B0A / 1000]
+        print("step 4", self.cjb_choiceA, self.cjb_choiceB)
+        return [self.cjb_choiceA, self.cjb_choiceB]
+
+    def average_firing_time_windows_cv(self):
+        cv_A_choiceA, cv_A_choiceB, cv_B_choiceA, cv_B_choiceB = [], [], [], []
+
+        # Initilisation of mean
+        mean_cv_0B1A, mean_cv_1B0A = 0, 0
+
+        for choice_i in self.list_choice:
+            for j in range(self.ΔB, 3, -4):
+                mean_cv_ij, mean_cv_ji = 0, 0
+                if len(self.result[(1, j, choice_i)][4]) != 0:
+                    for k in range(3000, 4001):
+                        mean_cv_ij += self.result[(1, j, choice_i)][4][k]
+                if len(self.result[(j, 1, choice_i)][4]) != 0:
+                    for k in range(3000, 4001):
+                        mean_cv_ji += self.result[(j, 1, choice_i)][4][k]
+                if choice_i == 'A':
+                    cv_A_choiceA.append(mean_cv_ij / 1000)
+                    cv_B_choiceA.append(mean_cv_ji / 1000)
+                elif choice_i == 'B':
+                    cv_A_choiceB.append(mean_cv_ij / 1000)
+                    cv_B_choiceB.append(mean_cv_ji / 1000)
+
+            if len(self.result[(1, 0, choice_i)][4]) == 0:
+                mean_cv_0B1A = 0
+            else:
+                for k in range(3000, 4001):
+                    mean_cv_0B1A += self.result[(1, 0, choice_i)][4][k]
+            if len(self.result[(0, 1, choice_i)][0]) == 0:
+                mean_cv_1B0A = 0
+            else:
+                for k in range(3000, 4001):
+                    mean_cv_1B0A += self.result[(0, 1, choice_i)][4][k]
+            if choice_i == 'A':
+                self.cv_choiceA = [mean_cv_0B1A / 1000] + cv_B_choiceA + cv_A_choiceA[::-1] + [mean_cv_1B0A / 1000]
+            else:
+                self.cv_choiceB = [mean_cv_0B1A / 1000] + cv_B_choiceB + cv_A_choiceB[::-1] + [mean_cv_1B0A / 1000]
+        print("step 4", self.cv_choiceA, self.cv_choiceB)
+        return [self.cv_choiceA, self.cv_choiceB]
 
     def pourcentage_B(self):
         """determination of pourcentage of choice B depending on quantity of each juice"""
@@ -244,11 +289,9 @@ class data_analysis_cells:
         for j in range(self.ΔB, 3, -4):
             total_choice_1 = self.choice_B[(1, j)] + self.choice_A[(1, j)]
             pourcentage_A_choice_B.append((self.choice_B[(1, j)] / (total_choice_1)) * 100)
-            print("total choice 1j", total_choice_1, self.choice_B[(1, j)])
 
             total_choice = self.choice_A[(j, 1)] + self.choice_B[(j, 1)]
             pourcentage_B_choice_B.append((self.choice_B[(j, 1)] / (total_choice)) * 100)
-            print("total choice j1", total_choice, self.choice_B[(j, 1)])
 
         total_choice_1 = self.choice_B[(1, 0)] + self.choice_A[(1, 0)]
         total_choice_2 = self.choice_B[(0, 1)] + self.choice_A[(0, 1)]
@@ -258,19 +301,18 @@ class data_analysis_cells:
 
     def average_firing_offerB_ov(self):
         """graph 4D"""
-        nb_4D = 0
-        firing_4D = 0
         for j in range(self.ΔB +1):
+            nb_4D = 0
+            firing_4D = 0
             for i in range(self.ΔA +1):
                 for choice_i in self.list_choice:
-                    if len(self.result[i, j, choice_i][0]) != 0:
+                    if len(self.result[(i, j, choice_i)][0]) != 0:
                         for k in range(2000, 3001):
                             firing_4D += self.result[(i, j, choice_i)][0][k]
                             nb_4D += 1
-            if nb_4D != 0:
-                self.firing_D.append(firing_4D / nb_4D)
-            else:
-                print("nb 4D = 0")
+            if nb_4D == 0:
+                nb_4D = 1
+            self.firing_D.append(firing_4D / nb_4D)
         print("step 6")
         return self.firing_D
 
@@ -280,13 +322,13 @@ class data_analysis_cells:
         firing_4H_A, firing_4H_B =0, 0
         for j in range(self.ΔB + 1):
             for i in range(self.ΔA + 1):
-                if len(self.result[(i, j, 'A')][1]) != 0:
+                if len(self.result[(i, j, 'A')][2]) != 0:
                     for k in range(2000, 3001):
-                        firing_4H_A += self.result[(i, j, 'A')][1][k]
+                        firing_4H_A += self.result[(i, j, 'A')][2][k]
                         nb_4H_A += 1
-                if len(self.result[(i,j,'B')][1]) != 0:
+                if len(self.result[(i,j,'B')][2]) != 0:
                     for k in range(2000, 3001):
-                        firing_4H_B += self.result[(i, j, 'B')][1][k]
+                        firing_4H_B += self.result[(i, j, 'B')][2][k]
                         nb_4H_B += 1
                 if nb_4H_A != 0:
                     self.firing_H_A.append(firing_4H_A / nb_4H_A)
@@ -301,28 +343,29 @@ class data_analysis_cells:
 
     def average_firing_chosen_value(self):
         """Get firing rate in function of chosen value"""
-        y_a, y_b = 0, 0
-        nb_Y_A, nb_Y_B = 0, 0
+
         for i in range(self.ΔA + 1):
             for j in range(self.ΔB + 1):
                 for l in range(len(self.result_one_trial[(i, j)])):
-                    if len(self.result_one_trial[(i,j)][l]) !=0 and self.result_one_trial[(i,j)][l][5] == 'A':
-                        self.X_A.append(i * 2)
-                        for k in range(3000, 4001):
-                            y_a += self.result_one_trial[(i, j)][l][4][k]
-                            nb_Y_A += 1
-                        self.Y_A.append(y_a / nb_Y_A)
-                    else :
-                        self.X_B.append(j)
-                        for k in range(3000, 4001):
-                            y_b += self.result_one_trial[(i, j)][l][4][k]
-                            nb_Y_B += 1
-                        self.Y_B.append(y_b / nb_Y_B)
+                    y_a, y_b = 0, 0
+                    nb_Y_A, nb_Y_B = 0, 0
+                    if len(self.result_one_trial[(i,j)][l]) !=0:
+                        if self.result_one_trial[(i,j)][l][5] == 'A':
+                            self.X_A.append(i * 2)
+                            for k in range(3000, 4001):
+                                y_a += self.result_one_trial[(i, j)][l][4][k]
+                                nb_Y_A += 1
+                            self.Y_A.append(y_a / nb_Y_A)
+                        else :
+                            self.X_B.append(j)
+                            for k in range(3000, 4001):
+                                y_b += self.result_one_trial[(i, j)][l][4][k]
+                                nb_Y_B += 1
+                            self.Y_B.append(y_b / nb_Y_B)
         print("step 8")
         return [self.X_A, self.Y_A, self.X_B, self.Y_B]
 
     def tuning_curve_ov(self):
-
         for i in range(self.ΔA + 1):
             for j in range(self.ΔB + 1):
                 if i != 0 and j != 0:
@@ -337,9 +380,9 @@ class data_analysis_cells:
                             for k in range(2000, 3001):
                                 mean_tuning_ov += self.result_one_trial[(i, j)][l][0][k]
                                 nb_tun_ov +=1
-                    if nb_tun_ov == 0:
-                        nb_tun_ov = 1
-                    self.tuning_ov.append((i, j, mean_tuning_ov / nb_tun_ov, c))
+                        if nb_tun_ov == 0:
+                            nb_tun_ov = 1
+                        self.tuning_ov.append((i, j, mean_tuning_ov / nb_tun_ov, c))
         print("step 9")
         return self.tuning_ov
 
@@ -359,9 +402,9 @@ class data_analysis_cells:
                             for k in range(2000, 3001):
                                 mean_tuning_cjb += self.result_one_trial[(i, j)][l][2][k]
                                 nb_tun_cjb +=1
-                    if nb_tun_cjb == 0:
-                        nb_tun_cjb = 1
-                    self.tuning_cjb.append((i, j, mean_tuning_cjb / nb_tun_cjb, c))
+                        if nb_tun_cjb == 0:
+                            nb_tun_cjb = 1
+                        self.tuning_cjb.append((i, j, mean_tuning_cjb / nb_tun_cjb, c))
         print("step 10")
         return self.tuning_cjb
 
@@ -380,11 +423,17 @@ class data_analysis_cells:
                             for k in range(2000, 3001):
                                 mean_tuning_cv += self.result_one_trial[(i, j)][l][4][k]
                                 nb_tun_cv += 1
-                    if nb_tun_cv == 0:
-                        nb_tun_cv = 1
-                    self.tuning_cv.append((i, j, mean_tuning_cv / nb_tun_cv, c))
+                        if nb_tun_cv == 0:
+                            nb_tun_cv = 1
+                        self.tuning_cv.append((i, j, mean_tuning_cv / nb_tun_cv, c))
         print("step 11")
         return self.tuning_cv
+
+
+
+
+
+
 
     def firing_cja_cjb(self):
         ria, rib = 0, 0

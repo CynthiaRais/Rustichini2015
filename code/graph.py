54f0391e520cc97add56ad2886bf36e53681e2b6
Cynthia Rais <cynthia.rais@sfr.fr>
2017-05-30 21:44:23 +0200

    Add graph for test of a trial

--- code/graph.py
+++ code/graph.py
@@ -16,13 +16,13 @@ class Graph:
         self.dt = dt
         self.analysis = analysis
         self.ov, self.cjb, self.cv = {}, {}, {}
-        self.X_axis = np.arange(0, self.t_exp, self.dt)
+        self.X_axis = np.arange(-0.5, 1.0, self.dt)
         X2_axis = ["0B: 1A", "1B: 20A", "1B: 16A", "1B: 12A", "1B: 8A", "1B: 4A", "4B: 1A", "8B: 1A", "12B: 1A",
                "16B: 1A", "20B: 1A", "1B: 0A"]
         bokeh.plotting.output_notebook()
 
     def firing_time_ov(self, Y):
-        figure_4_A = bokeh.plotting.figure(title="Figure 4 A", plot_width=700, plot_height=700)
+        figure_4_A = bokeh.plotting.figure(title="Figure 4 A", plot_width=300, plot_height=300)
         figure_4_A.multi_line([self.X_axis, self.X_axis, self.X_axis],
                               Y,
                               color=['red', "green", "blue"])
@@ -33,7 +33,7 @@ class Graph:
 
 
     def firing_specific_set_ov(self, ov_choice, pourcentage_choice_B):
-        figure_4_C = bokeh.plotting.figure(title="Figure 4 C", plot_width=700, plot_height=700)
+        figure_4_C = bokeh.plotting.figure(title="Figure 4 C", plot_width=300, plot_height=300)
         figure_4_C.diamond(x=range(0, 6), y=ov_choice[0][:6], color='red', size=10)
         figure_4_C.circle(x=range(6, 12), y=ov_choice[1][6:], color="blue", size=10)
         figure_4_C.circle(x=range(12), y=pourcentage_choice_B, color="black", size=10)
@@ -42,13 +42,13 @@ class Graph:
 
     def firing_offer_B(self, firing_D):
         print("firing_D", len(firing_D))
-        figure_4_D = bokeh.plotting.figure(title="Figure 4 D", plot_width=700, plot_height=700)
-        figure_4_D.annulus(x=range(20), y=firing_D, color="purple", inner_radius=0.2, outer_radius=0.5)
+        figure_4_D = bokeh.plotting.figure(title="Figure 4 D", plot_width=300, plot_height=300)
+        figure_4_D.annulus(x=range(21), y=firing_D, color="purple", inner_radius=0.1, outer_radius=0.2)
         bokeh.plotting.show(figure_4_D)
 
 
     def firing_time_cjb(self, Y):
-        figure_4_E = bokeh.plotting.figure(title="Figure 4 E", plot_width=700, plot_height=700)
+        figure_4_E = bokeh.plotting.figure(title="Figure 4 E", plot_width=300, plot_height=300)
         figure_4_E.multi_line([self.X_axis, self.X_axis], Y,
                           color=['red', "blue"])
         bokeh.plotting.show(figure_4_E)
@@ -59,7 +59,7 @@ class Graph:
 
 
     def firing_specific_set_cjb(self, cj_choice, pourcentage_choice_B):
-        figure_4_G = bokeh.plotting.figure(title="Figure 4 G", plot_width=700, plot_height=700)
+        figure_4_G = bokeh.plotting.figure(title="Figure 4 G", plot_width=300, plot_height=300)
         figure_4_G.diamond(x=range(0, 6), y=cj_choice[0][:6], color='red', size=10)
         figure_4_G.circle(x=range(6, 12), y=cj_choice[1][6:], color="blue", size=10)
         figure_4_G.circle(x=range(12), y=pourcentage_choice_B, color="black", size=10)
@@ -68,14 +68,14 @@ class Graph:
 
     def firing_choice(self, firing_H):
         Y = firing_H
-        figure_4_H = bokeh.plotting.figure(title="Figure 4 H", plot_width=700, plot_height=700)
+        figure_4_H = bokeh.plotting.figure(title="Figure 4 H", plot_width=300, plot_height=300)
         figure_4_H.diamond(x=[1 for i in range(len(Y[0]))], y=Y[0], color="red")
         figure_4_H.circle(x=[2 for i in range(len(Y[1]))], y=Y[1], color="blue")
         bokeh.plotting.show(figure_4_H)
 
 
     def firing_time_cv(self, Y):
-        figure_4_I = bokeh.plotting.figure(title="Figure 4 I", plot_width=700, plot_height=700)
+        figure_4_I = bokeh.plotting.figure(title="Figure 4 I", plot_width=300, plot_height=300)
         figure_4_I.multi_line([self.X_axis, self.X_axis, self.X_axis],
                               Y,
                               color=['red', "green", "blue"])
@@ -87,31 +87,40 @@ class Graph:
 
 
     def firing_specific_set_cv(self, cv_choice, pourcentage_choice_B):
-        figure_4_K = bokeh.plotting.figure(title="Figure 4 K", plot_width=700, plot_height=700)
+        figure_4_K = bokeh.plotting.figure(title="Figure 4 K", plot_width=300, plot_height=300)
         figure_4_K.diamond(x=range(0, 6), y=cv_choice[0][:6], color='red', size=10)
         figure_4_K.circle(x=range(6, 12), y=cv_choice[1][6:], color="blue", size=10)
         figure_4_K.circle(x=range(12), y=pourcentage_choice_B, color="black", size=10)
         bokeh.plotting.show(figure_4_K)
 
     def firing_chosen_value(self, Y):
-        figure_4_L = bokeh.plotting.figure(title="Figure 4 L", plot_width=700, plot_height=700)
+        figure_4_L = bokeh.plotting.figure(title="Figure 4 L", plot_width=300, plot_height=300)
         figure_4_L.diamond(x=Y[0], y=Y[1], color="red", size=10)
-        figure_4_L.circle(x=Y[2], y=[3], color="blue", size=10)
+        figure_4_L.circle(x=Y[2], y=Y[3], color="blue", size=10)
         bokeh.plotting.show(figure_4_L)
 
+    def logistic_regression(self, X):
+        graphs.tuningcurve(X,)
 
     def cja_cjb(self):
         figure_9A = bokeh.plotting.figure(title="Figure 9 A", plot_width=700, plot_height=700)
 
 
+    def test(self, firing_cj, firing_cv):
+        figure_test_cj = bokeh.plotting.figure(title="Figure test cj a", plot_width=300, plot_height=300)
+        figure_test_cj.line(x=np.arange(-1, 1, 0.0005 ), y=firing_cj, color="red")
+        figure_test_cv = bokeh.plotting.figure(title="Figure test cj a", plot_width=300, plot_height=300)
+        figure_test_cv.line(x=np.arange(-1, 1, 0.0005), y=firing_cv, color="red")
+        bokeh.plotting.show(figure_test_cj)
+        bokeh.plotting.show(figure_test_cv)
 
     def figure_4(self):
-        figure_test_cj_a = bokeh.plotting.figure(title="Figure test cj a", plot_width=700, plot_height=700)
-        figure_test_cj_b = bokeh.plotting.figure(title="Figure test cj b", plot_width=700, plot_height=700)
-        figure_test_ns = bokeh.plotting.figure(title="Figure test ns", plot_width=700, plot_height=700)
-        figure_test_cv = bokeh.plotting.figure(title="Figure test cv", plot_width=700, plot_height=700)
 
-        figure_test_cj_a.multi_line([self.X_axis, self.X_axis, self.X_axis], self.analysis.test_cj_a, color=["red", "blue", "green"])
+        figure_test_cj_b = bokeh.plotting.figure(title="Figure test cj b", plot_width=300, plot_height=300)
+        figure_test_ns = bokeh.plotting.figure(title="Figure test ns", plot_width=300, plot_height=300)
+        figure_test_cv = bokeh.plotting.figure(title="Figure test cv", plot_width=300, plot_height=300)
+
+        #figure_test_cj_a.multi_line([self.X_axis, self.X_axis, self.X_axis], self.analysis.test_cj_a, color=["red", "blue", "green"])
         figure_test_cj_b.multi_line([self.X_axis, self.X_axis, self.X_axis], self.analysis.test_cj_b, color=["red", "blue", "green"])
         figure_test_ns.multi_line([self.X_axis, self.X_axis,self. X_axis], self.analysis.test_ns, color=["red", "blue", "green"])
         figure_test_cv.multi_line([self.X_axis, self.X_axis, self.X_axis], self.analysis.test_cv, color=["red", "blue", "green"])
@@ -129,7 +138,7 @@ class Graph:
         #bokeh.plotting.show(figure_4_K)
         #bokeh.plotting.show(figure_4_L)
 
-        bokeh.plotting.show(figure_test_cj_a)
+        #bokeh.plotting.show(figure_test_cj_a)
         bokeh.plotting.show(figure_test_cj_b)
         bokeh.plotting.show(figure_test_ns)
         bokeh.plotting.show(figure_test_cv)

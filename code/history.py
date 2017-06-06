42ca33cc0a920c2c85eebbac6e8b78fc3cc7f9ce
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2017-06-07 00:00:35 +0200

    Better replicability test

--- code/history.py
+++ code/history.py
@@ -5,10 +5,15 @@ class TrialHistory:
 
     def __init__(self):
 
-        self.r_ova, self.r_ovb = [], []
-        self.r_cja, self.r_cjb = [], []
-        self.r_ns, self.r_cv   = [], []
+        self.data = {name: [] for name in ['r_cja', 'r_cjb', 'r_ns', 'r_cv',
+                                           'S_ampa_cja', 'S_ampa_cjb', 'S_ampa_ns',
+                                           'S_ndma_cja', 'S_ndma_cjb', 'S_ndma_ns',
+                                           'S_gaba']}
 
-        self.S_ampa_cja, self.S_ampa_cjb, self.S_ampa_ns = [], [], []
-        self.S_ndma_cja, self.S_ndma_cjb, self.S_ndma_ns = [], [], []
-        self.S_gaba = []
+
+    def __getattr__(self, name):
+        return self.data[name]
+
+        # self.r_ova, self.r_ovb = [], []
+        # self.r_cja, self.r_cjb = [], []
+        # self.r_ns, self.r_cv   = [], []

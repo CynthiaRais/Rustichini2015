a4b181c3a493e79d28b4ac40c2467426abcc2018
Cynthia Rais <cynthia.rais@sfr.fr>
2017-06-16 13:52:34 +0200

    Tests for some conditions

--- tests/test_replicable.py
+++ tests/test_replicable.py
@@ -21,15 +21,15 @@ def access_data(datamat, key):
 class ReplicableTests(unittest.TestCase):
     """Test replicability"""
 
-    def test_replicable(self, verbose=VERBOSE):
+    def test_replicable(self, verbose=VERBOSE, x_a=1,x_b=0):
         """Test that the matlab code remains """
-        K = 100
+        K = 6101
 
         datamat_ref = scipy.io.loadmat('testdata_ref.mat')
-        datamat     = scipy.io.loadmat('testdata.mat')
+        datamat     = scipy.io.loadmat('testdata{}A{}B.mat'.format(x_a, x_b))
         model = Model(range_A=[0, 15], range_B=[0, 15], random_seed=0, t_exp=(100+K)*0.0005, σ_eta=0,
                       full_log=True)
-        model.one_trial(1, 10)
+        model.one_trial(x_a, x_b)
 
         if verbose:
             for key_mat, key_py in [('sampa1', 'S_ampa_1'), ('nu1', 'r_1'), ('Iamparec1', 'I_ampa_rec_1'),
@@ -42,6 +42,10 @@ class ReplicableTests(unittest.TestCase):
         np.allclose(access_data(datamat, 'sampa1')[:K], model.trial_history.S_ampa_1[:K])
         np.allclose(access_data(datamat, 'nu1')[:K], model.trial_history.r_1[:K])
         np.allclose(access_data(datamat, 'nuI')[:K], model.trial_history.r_I[:K])
+
+    def test_replication(self):
+        for (x_a,x_b) in [(1,0), (1,10), (4,1), (5,10)]:
+            self.test_replicable(x_a = x_a, x_b = x_b)
 
 
 if __name__ == '__main__':

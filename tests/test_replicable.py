f59e10b80ccbf441524c4714eaba230e2f205c73
Fabien C. Y. Benureau <fabien@benureau.com>
2018-04-12 00:39:48 +0900

    Update tests: better feedback, more strenuous

--- tests/test_replicable.py
+++ tests/test_replicable.py
@@ -14,7 +14,7 @@ import dotdot
 from neuromodel import QuantitativelyReplicatedModel
 
 
-VERBOSE  = False  # useful output when mismatch are detected.
+VERBOSE  = True  # useful output when mismatch are detected.
 FULL_LOG = False  # checks every variable, not only r_ovb, r_2, r_3 and r_I
                   # (requires full_log matlab files)
 
@@ -28,11 +28,37 @@ def first_mismatch(a, b, rtol=1e-07, atol=1e-07):
             return k
     return None
 
+def _display_differences(key, data_mat, data_py, k, rtol, atol, backtrack=3, forward=7, precision=10):
+    k_prev = max(0, k - backtrack)
+    length = backtrack + forward
+    k_key = first_mismatch(data_py, data_mat, rtol=rtol, atol=atol)
+    print('{}: t={}-{} (first mismatch t={})'.format(key, k_prev, k_prev + length, k_key))
+
+    subset_mat = data_mat[k_prev:k_prev+length]
+    subset_py  =  data_py[k_prev:k_prev+length]
+    mat_str, py_str = [], []
+    fmt = '{' + ': {}.{}f'.format(precision + 4, precision) + '}'
+    for i, (e_mat, e_py) in enumerate(zip(subset_mat, subset_py)):
+        e_mat_str = fmt.format(e_mat)
+        e_py_str  = fmt.format(e_py)
+        diffs = [i for i, (a, b) in enumerate(zip(e_mat_str, e_py_str)) if a != b]
+        d = diffs[0] if len(diffs) > 0 else len(e_mat_str)
+        if not np.allclose(e_mat, e_py, rtol=rtol, atol=atol):
+            mat_str.append('\033[38;5;2m{}\033[38;5;1m{}\033[0m'.format(e_mat_str[:d], e_mat_str[d:]))
+            py_str.append( '\033[38;5;2m{}\033[38;5;1m{}\033[0m'.format(e_py_str[:d],  e_py_str[d:]))
+        else:
+            mat_str.append('\033[38;5;2m{}\033[38;5;3m{}\033[0m'.format(e_mat_str[:d], e_mat_str[d:]))
+            py_str.append( '\033[38;5;2m{}\033[38;5;3m{}\033[0m'.format(e_py_str[:d],  e_py_str[d:]))
+
+    print('matlab: {}'.format(', '.join(mat_str)))
+    print('python: {}'.format(', '.join(py_str)))
+
 
 class TestReplicability(unittest.TestCase):
     """Test replicability"""
 
-    def _aux_test_replicable(self, x_a=5, x_b=10, rtol=0.0, atol=1e-8, # rtol=1e-9, atol=0,
+
+    def _aux_test_replicable(self, x_a=5, x_b=10, rtol=0.0, atol=1e-10, # rtol=1e-9, atol=0,
                                   verbose=VERBOSE, full_log=FULL_LOG, prefix='', model_args={}):
         """Test that QuantitativelyReplicatedModel reproduce exactly the same sequence of values
         as the Matlab code obtained from Aldo Rustichini and Camillo Padoa-Schioppa."""
@@ -61,11 +87,20 @@ class TestReplicability(unittest.TestCase):
                                 'S_gaba',
                                 'I_eta_1', 'I_eta_2', 'I_eta_3', 'I_eta_I',
                                 'I_ampa_rec_3', 'I_ampa_rec_I',
-                                'I_syn_1', 'I_syn_2', 'I_syn3', 'I_syn_I',
+                                'I_syn_1', 'I_syn_2', 'I_syn_3', 'I_syn_I',
                                 'I_stim_1', 'I_stim_2']
 
             if verbose:
                 no_mismatch = []
+
+                k_min = float('inf')
+                for key in key_compare:
+                    data_py  = getattr(model.trial_history, key)[:K:slicesize]
+                    data_mat = access_data(datamat, key)[:Kslice]
+                    k = first_mismatch(data_py, data_mat, rtol=rtol, atol=atol)
+                    if k is not None:
+                        k_min = min(k_min, k)
+
                 for key in key_compare:
                     data_py  = getattr(model.trial_history, key)[:K:slicesize]
                     data_mat = access_data(datamat, key)[:Kslice]
@@ -73,10 +108,9 @@ class TestReplicability(unittest.TestCase):
                     if k is None:
                         no_mismatch.append(key)
                     else:
-                        k_prev = max(0, k - 3)
-                        print('{}: t={}-{} (first mismatch t={})'.format(key, k_prev, k_prev+10, k))
-                        print('matlab: {}'.format(', '.join('{: 14.10f}'.format(e) for e in data_mat[k_prev:k_prev+10])))
-                        print('python: {}'.format(', '.join('{: 14.10f}'.format(e) for e in  data_py[k_prev:k_prev+10])))
+                        _display_differences(key, data_mat, data_py, k_min, rtol, atol,
+                                             backtrack=5, forward=4, precision=20)
+
                 print('No mismatch for: {}'.format(', '.join(no_mismatch)))
 
             for key in key_compare:
@@ -123,7 +157,7 @@ class TestReplicability(unittest.TestCase):
     def test_fig7_replicability(self, offers=[(1, 2), (3, 10), (10, 20)]):
         for x_a, x_b in offers:
             self._aux_test_replicable(x_a, x_b, prefix='fig7_EF_',
-                model_args={'ΔA': (0, 10), 'w_p': 1.82})
+                model_args={'ΔA': (0, 10), 'w_p': 1.82}, atol=1e-11)
 
 if __name__ == '__main__':
     unittest.main()
